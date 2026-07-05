f94e441ea092722a24e017852d1ec55aa84d7da2
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-07-05 14:16:53 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (68.72%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int d) {
+        int n= nums.size();
+        d%=n;
+
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+d);
+        reverse(nums.begin()+d,nums.end());
+       
+    }
+};
