040fbbf281d7ecf4d6be743b8e0168b8235a24c0
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-07-06 16:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.60%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        int freq[26] = {0};
+        for(char ch:s)
+        {
+            freq[ch-'a']++;
+        }
+        for(char ch:t)
+        {
+            freq[ch-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
