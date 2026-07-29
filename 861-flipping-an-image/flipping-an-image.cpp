1e29a16b030a4d4414be09192dfd89ee0a459976
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-07-29 13:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (36.83%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int left = 0;
+            int right = n-1;
+
+            while(left<right)
+            {
+                swap(image[i][left],image[i][right]);
+                left++;
+                right--;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(image[i][j]==1)
+                {
+                    image[i][j]=0;
+                }
+                else
+                {
+                    image[i][j]=1;
+                }
+            }
+        }
+
+        return image;
+    }
+};
