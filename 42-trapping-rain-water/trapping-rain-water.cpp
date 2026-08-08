f5046236cccc4330d8c3000ada572d146b5390ba
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-08-08 12:30:25 +0530

    Time: 4 ms (8.81%) | Memory: 27.2 MB (27.20%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int n = height.size();
+
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+
+        // Build leftMax
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
+        }
+
+        // Build rightMax
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+
+        // Calculate trapped water
+        int water = 0;
+
+        for (int i = 0; i < n; i++) {
+            int waterHeight = min(leftMax[i], rightMax[i]);
+
+            water += waterHeight - height[i];
+        }
+
+        return water;
+    }
+};
