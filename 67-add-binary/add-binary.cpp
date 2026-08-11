7e7619e1d7c8af21cf9dcae93c0becdce1399de3
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-08-11 11:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (58.12%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+
+        int carry = 0;
+
+        string ans = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            ans += char('0' + (sum % 2));
+
+            carry = sum / 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
