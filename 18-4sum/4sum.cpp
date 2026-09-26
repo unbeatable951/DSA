403d656128c020bdd89fafde0b127c49d6c258ac
unbeatable951 <163458526+unbeatable951@users.noreply.github.com>
2026-09-26 14:29:35 +0530

    Time: 1 ms (98.18%) | Memory: 17.4 MB (87.47%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        std::vector<std::vector<int>> ans;
+        int n = nums.size();
+        if (n < 4) return ans;
+
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // Cast to long long to prevent integer overflow
+            if ((long long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if ((long long)nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                if ((long long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if ((long long)nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        ++left;
+                        --right;
+                        while (left < right && nums[left] == nums[left - 1]) ++left;
+                        while (left < right && nums[right] == nums[right + 1]) --right;
+                    } else if (sum < target) {
+                        ++left;
+                    } else {
+                        --right;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
